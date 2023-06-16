0cb3d8b747e1ecf7520b44121502e626d85c864e
chamarabandarawd <chamarabandarawd@gmail.com>
2023-06-16 09:53:41 +0530

    Greething card done

--- src/main/java/AppInitializer.java
+++ src/main/java/AppInitializer.java
@@ -10,6 +10,7 @@ public class AppInitializer extends Application {
     @Override
     public void start(Stage primarystage) throws Exception {
         primarystage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/MainView.fxml"))));
+        primarystage.setTitle("Main view");
         primarystage.show();
     }
 }
--- src/main/java/controller/MainViewController.java
+++ src/main/java/controller/MainViewController.java
@@ -1,4 +1,18 @@
 package controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
 public class MainViewController {
+    public Text lbGreething;
+    public TextField txtName;
+    public Button btnOk;
+
+    public void btnOkOnClicked(MouseEvent mouseEvent) {
+        String text = txtName.getText();
+        lbGreething.setText("Good Morning "+text+" !...");
+
+    }
 }
